7567c6f45178d542b7aab6931f913d1f32b31daa
Charles Law <charles.law@thegreenstone.com>
2017-05-11 11:08:29 -0400

    quick

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(function() {
         clientId: 'e9a5812bcb164d4f8549c5e8080b4d4a',
         accessToken: '2010844114.1677ed0.9c7dd4b91bac42d39b4fc31e90f8e770',
         links: true,
-        limit: 8,
+        limit: 12,
         sortBy: 'most-recent',
         resolution: 'low_resolution',
         template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a></div></div></div>'
