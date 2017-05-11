0defadc0e431fb05c0e6052cab371198093ce27b
Charles Law <charles.law@thegreenstone.com>
2017-05-11 09:57:43 -0400

    installed cleanup

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ $(function() {
  
     //Set up instafeed
     var feed = new Instafeed({
-        clientId: '97ae5f4c024c4a91804f959f43f2635f',
-        target: 'instafeed',
-        get: 'tagged',
-        tagName: 'photographyportfolio',
+        get: 'user',
+        userId: '2010844114',
+        clientId: 'e9a5812bcb164d4f8549c5e8080b4d4a',
+        accessToken: '2010844114.1677ed0.9c7dd4b91bac42d39b4fc31e90f8e770',
         links: true,
         limit: 8,
         sortBy: 'most-recent',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+        resolution: 'low_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a></div></div></div>'
     });
     feed.run();
  
